ec8c811852678a411261d9b84cb986bee27f0f2b
Puka-jpg <pukar369@gmail.com>
2024-12-19 23:19:52 +0545

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import StarRating from "./Star_Rating/StarRating";
+
+import "./App.css";
+
+function App() {
+  return <StarRating noOfStars={10} />;
+}
+
+export default App;
